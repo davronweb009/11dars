7c738079184885e098594501d97946c2ae10103c
davronweb009 <davronweb07@mail.ru>
2025-02-07 13:57:50 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+let user = prompt('bacround color')
+let user1 = prompt('sozni rangi')
+if(user){
+    document.body.style.backgroundColor = user
+}else{
+    alert('siz tanlagan rang')
+}
+if(user1){
+    text.style.color = user1
+}else{
+    alert('siz tanlagan rang')
+}
